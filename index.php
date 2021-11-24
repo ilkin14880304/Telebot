a8942a1e077561d606ccb1a38ef44d6ba3cd43f3
ilkin14880304 <49369916+ilkin14880304@users.noreply.github.com>
2021-11-24 14:09:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <? PHP
 define('BOT_TOKEN', '2113158312:AAFXWI22tksl4w4gBbm2rlaQSyyk7Jt3cI4'); 
-define('WEBHOOK_URL', 'https://github.com/ilkin14880304/Telebot.git');
+define('WEBHOOK_URL', 'https://git.heroku.com/mighty-plateau-22089.git');
 ?>
